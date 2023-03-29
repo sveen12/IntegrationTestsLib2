214aed9cd33f8d21351877337c7a24fa08fff153
sveen12 <sveen12@gmail.com>
2023-03-29 10:54:09 -0500

    Update

--- integration-testing.ps1
+++ integration-testing.ps1
@@ -1,2 +1,2 @@
 docker-compose -p microservices-environment up -d
-($env:ServicesUrl__MultiplicationService="http://localhost:8002") | dotnet test
+($env:ServicesUrl__MultiplicationService="http://localhost:8002") | dotnet test ../../
